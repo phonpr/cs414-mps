98540953fc8b9849d30b5f4c6c2d0b3395994b76
Seung H. Cha <phonpr@outlook.com>
2014-02-07 14:03:18 -0600

    spike code for cs 414 mp1

---
+++ Mp1/src/org/cs414/mp1/Monitor.java
@@ -0,0 +1,5 @@
+package org.cs414.mp1;
+
+public class Monitor {
+
+}
---
+++ Mp1/src/org/cs414/mp1/Mp1.java
@@ -0,0 +1,30 @@
+package org.cs414.mp1;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Mp1 {
+
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		JFrame frame = new JFrame("HelloWorldSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+}

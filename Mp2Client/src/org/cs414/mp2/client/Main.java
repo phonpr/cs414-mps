1ee8fc4223612d3f0ac6d911dcb0a5673b9e7dd4
Seung H <phonpr@outlook.com>
2014-04-03 23:57:23 -0500

    initial setup for MP2, client and server

---
+++ Mp2Client/src/org/cs414/mp2/client/Main.java
@@ -0,0 +1,10 @@
+package org.cs414.mp2.client;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Mp2Server/src/org/cs414/mp2/server/Main.java
@@ -0,0 +1,10 @@
+package org.cs414.mp2.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

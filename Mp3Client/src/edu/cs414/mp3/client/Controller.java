905e75d8248fde39a916eb2e224cd3454086f2d5
Seung H. Cha <phonpr@outlook.com>
2014-05-02 23:30:58 -0500

    deleted unnecessarry files

--- Mp3Client/src/edu/cs414/mp3/client/Controller.java
+++
@@ -1,9 +0,0 @@
-package edu.cs414.mp3.client;
-
-public interface Controller {
-	public void onPlay();
-	public void onStop();
-	public void onPause();
-	public void onResume();
-	public void onMute();
-}
--- Mp3Common/src/edu/cs414/mp3/common/Constants.java
+++
@@ -1,5 +0,0 @@
-package edu.cs414.mp3.common;
-
-public class Constants {
-
-}

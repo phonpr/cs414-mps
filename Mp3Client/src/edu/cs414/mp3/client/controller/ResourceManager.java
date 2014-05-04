ef5cb67378d08b251fc3552cb2a9048c6a4a63db
Seung H. Cha <phonpr@outlook.com>
2014-05-03 22:36:48 -0500

    resource manager

---
+++ Mp3Client/src/edu/cs414/mp3/client/controller/ResourceManager.java
@@ -0,0 +1,90 @@
+package edu.cs414.mp3.client.controller;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import edu.cs414.mp3.client.connection.DesktopConnection;
+import edu.cs414.mp3.client.connection.WebcamConnection;
+
+public class ResourceManager {
+	private static long currentResource = 0;
+	private static boolean running = false;
+	
+	private static DesktopConnection desktopConnection = null;
+	private static WebcamConnection webcamConnection = null;
+	
+	private class ResourceManagerThread implements Runnable {
+		@Override
+		public void run() {
+			boolean changed = false;
+			
+			while (running) {
+				long newResource = Long.parseLong(readFile("resource.txt"));
+				System.out.println("[ResourceManager] reading resource : " + newResource);
+				
+				if (currentResource != newResource) {
+					System.out.println("[ResourceManager] resource changed from " + currentResource + " => " + newResource);
+					currentResource = newResource;
+					changed = true;
+				}
+				
+				if (desktopConnection != null && changed) {
+					desktopConnection.onClientResourceChanged(currentResource);
+				}
+				
+				if (webcamConnection != null && changed) {
+					webcamConnection.onClientResourceChanged(currentResource);
+				}
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	private static ResourceManagerThread resourceRunnable;
+
+	public static void start() {
+		resourceRunnable = new ResourceManager().new ResourceManagerThread();
+		running = true;
+		new Thread(resourceRunnable).start();
+	}
+
+	public static void setDesktopConnection(DesktopConnection desktopConnection) {
+		ResourceManager.desktopConnection = desktopConnection;
+	}
+
+	public static void setWebcamConnection(WebcamConnection webcamConnection) {
+		ResourceManager.webcamConnection = webcamConnection;
+	}
+	
+	private static String readFile(String file) {
+		String value = "";
+		
+		try {
+			FileInputStream fileStream = new FileInputStream(file);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(fileStream));
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				value += line;
+			}
+			
+			fileStream.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return value;
+	}
+}

42549c8096f380c7a200d5fff6ee1e182ec8295f
Seung H. Cha <phonpr@outlook.com>
2014-05-04 15:46:20 -0500

    argument handling

--- Mp3Common/src/edu/cs414/mp3/common/ConnectionConfig.java
+++ Mp3Common/src/edu/cs414/mp3/common/ConnectionConfig.java
@@ -2,10 +2,18 @@ package edu.cs414.mp3.common;
 
 public class ConnectionConfig {
 	
-	public static final String WEBCAM_SERVER_HOST	= "127.0.0.1";
-	public static final int WEBCAM_SERVER_PORT		= 9000;
+	public static String WEBCAM_SERVER_HOST		= "127.0.0.1";
+	public static String DESKTOP_SERVER_HOST	= "127.0.0.1";
 	
-	public static final String DESKTOP_SERVER_HOST	= "127.0.0.1";
+	public static void setWebcamServerHost(String host) {
+		WEBCAM_SERVER_HOST = host;
+	}
+	
+	public static void setDesktopServerHost(String host) {
+		DESKTOP_SERVER_HOST = host;
+	}
+	
+	public static final int WEBCAM_SERVER_PORT		= 9000;
 	public static final int DESKTOP_SERVER_PORT		= 9001;
 	
 	public static final int DESKTOP_VIDEO_UDP_SINK	= 5000;
